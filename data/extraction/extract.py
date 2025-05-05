27f994d19aae6df0849d22ef877da75749be3b86
Armand Violle <74250225+armandviolle@users.noreply.github.com>
2025-05-05 11:00:29 +0200

    Included loading dictionary in script extract.py for simplicity

--- data/extraction/extract.py
+++ data/extraction/extract.py
@@ -1,6 +1,25 @@
 from datasets import load_dataset, Dataset, concatenate_datasets
 
 
+
+DATASETS = {
+  "SIMSAMU": {
+    "path": "medkit/simsamu",
+    "type": "text",
+    "split": "train", 
+    "remove_columns": ["schemaVersion", "monologues"],
+  },
+  "WMT-16": {
+    "path": "qanastek/WMT-16-PubMed", 
+    "type": "translation",
+    "files": "en-fr", 
+    "split": "train",
+    "remove_columns": [c for c in wmt.column_names if c != "translation"],
+  }
+}
+
+
+
 def extract_texts(example):
   texts = [" ".join([t["text"] for t in mono["terms"]])
     for mono in example["monologues"]]
