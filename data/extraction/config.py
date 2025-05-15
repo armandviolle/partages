0fce578164b9ecdeeae5c5a1c82414f6b123f136
Armand Violle <74250225+armandviolle@users.noreply.github.com>
2025-05-15 17:04:05 +0200

    Update config.py (datasets name normalization: capital characters and no special characters)

--- data/extraction/config.py
+++ data/extraction/config.py
@@ -1,4 +1,5 @@
-"SIMSAMU": {
+data_register = {
+    "SIMSAMU": {
         "path": "medkit/simsamu", 
         "name": None,
         "data_files": None,
@@ -6,7 +7,7 @@
         # "text_column": "monologues",
         "additional_processing": True
     }, 
-    "WMT-16": {
+    "WMT16": {
         "data_path": "qanastek/WMT-16-PubMed", 
         "name": "en-fr",
         "data_files": None,
@@ -14,7 +15,7 @@
         # "text_column": "translation",
         "additional_processing": True 
     }, 
-    "DEFT-2021": {
+    "DEFT2021": {
         "path": "DrBenchmarck/DEFT2021", 
         "name": None,
         "data_files": None,
