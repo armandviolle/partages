8d54043b81fffa5d0c95e85759bcb2d409fb8462
Armand Violle <74250225+armandviolle@users.noreply.github.com>
2025-05-15 16:45:52 +0200

    change "splits" key to "split" in config.py to support both str and list types

--- data/extraction/config.py
+++ data/extraction/config.py
@@ -2,25 +2,25 @@
         "path": "medkit/simsamu", 
         "name": None,
         "data_files": None,
-        "splits": ["train"],
-        "text_column": "monologues",
-        "additional_processing": True # cf fonction de Stéphane
+        "split": "train",
+        # "text_column": "monologues",
+        "additional_processing": True
     }, 
     "WMT-16": {
         "data_path": "qanastek/WMT-16-PubMed", 
         "name": "en-fr",
         "data_files": None,
-        "splits": ["train"]
-        "text_column": "translation",
-        "additional_processing": True # extraction du texte des clés "fr" dans la dico de chaque ligne
+        "split": "train",
+        # "text_column": "translation",
+        "additional_processing": True 
     }, 
     "DEFT-2021": {
         "path": "DrBenchmarck/DEFT2021", 
         "name": None,
         "data_files": None,
-        "splits": ["train", "validation", "test"], 
-        "text_column": "tokens",
-        "additional_processing": True # 1. On row-level, join lists on " " and 2. Concatenate rows with identical document_id, separated by "\n" or " ".
+        "split": ["train", "validation", "test"], 
+        # "text_column": "tokens",
+        "additional_processing": True
     }
     # Continue...
 }
