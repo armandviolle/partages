a19c0c2c5556d71a8fb4c930eb21923aaac84d58
Stephane <stephane.ohayon@sorbonne-universite.fr>
2025-07-08 10:23:04 +0200

    pxcorpus type hint and docstring numpy style

--- loaders/pxcorpus.py
+++ loaders/pxcorpus.py
@@ -1,16 +1,30 @@
+from typing import Dict, List, Optional
 from datasets import Dataset
 from .base_loader import BaseLoader
 
 class PXCORPUS(BaseLoader):
-    def postprocess(self, dataset, subset, split):
-        """
-        Processes the raw Hugging Face dataset OR locally loaded data for Mantra GSC French.
-        If local: 'dataset' will be the list of dicts from load_data.
-        If HF: 'dataset' is expected to be loaded via:
-        datasets.load_dataset(path="bigbio/mantra_gsc", name="mantra_gsc_fr_source", split=split)
+    """Loader for the PxCorpus dataset"""
+
+    def postprocess(self, dataset: Dataset, subset: Optional[str] = None, split: str = "train") -> Dataset:
+        """Format the raw dataset to a common schema.
+
+        Parameters
+        ----------
+        dataset : Dataset
+            The input dataset to postprocess.
+        subset : str, optional
+            Name of the subset being processed. None by default.
+        split : str
+            Name of the split being processed. Defaults to "train".
+
+        Returns
+        -------
+        Dataset
+            The postprocessed dataset with "text", "source", "subset",
+            and "source_split" columns.
         """
         txt = dataset['text'][0].splitlines()
-        res = {
+        res: Dict[str, List] = {
             "text": list(txt),
             "source": [self.source] * len(dataset) * len(txt),
             "subset": [subset] * len(dataset) * len(txt),
