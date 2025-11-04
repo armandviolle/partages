ccc7b41cc2fce2e89d834d9f03dca93ce8ab38a0
Vivano <armandviolle@gmail.com>
2025-11-04 14:53:13 +0100

    Process du lecture du dataset sourcé + création du dataset dédupliqué au sein d’un bloc éphémère (with) pour une meilleure gestion de l’espace disque / mémoire.
    Modification du dataset dédupliqué:
    - retrait des colonnes [source, subset, source_split] (moins de sens après déduplication et ça permet d’alléger le volume).
    - shuffle des lignes pour le nouveau dataset afin de favoriser un sharding plus homogène (cf. mail d’Aidan).
    
    Ajout docstrings et commentaires.

--- src/deduplicate.py
+++ src/deduplicate.py
@@ -1,4 +1,5 @@
 import datetime
+import gc
 import logging
 
 from huggingface_hub import HfFolder, login
@@ -19,6 +20,19 @@ logger = logging.getLogger(__name__)
 def post_process(
     dataset: DatasetDict | Dataset | IterableDatasetDict | IterableDataset,
 ) -> DatasetDict | Dataset | IterableDatasetDict | IterableDataset:
+    """
+    Post-process the dataset to remove duplicate entries based on the "text" field, and clean up unnecessary columns.
+
+    Parameters
+    ----------
+    dataset: DatasetDict | Dataset | IterableDatasetDict | IterableDataset
+        The input dataset to be deduplicated.
+
+    Returns
+    -------
+    DatasetDict | Dataset | IterableDatasetDict | IterableDataset
+        The deduplicated dataset with duplicate entries removed.
+    """
     seen_hashes = set()
 
     def is_unique_text(example):
@@ -30,25 +44,25 @@ def post_process(
 
     unique_data = dataset.filter(is_unique_text)
 
-    return unique_data
+    return unique_data.remove_columns(["source", "subset", "source_split"])
 
 
 def main():
+    """Main function to deduplicate a dataset and push the cleaned version to the Hugging Face Hub."""
     args = parse()
 
+    # Create logger instance
     if args.log_level == "DEBUG":
         log_level = logging.DEBUG
     elif args.log_level == "INFO":
         log_level = logging.INFO
     else:
         raise ValueError(args.log_level)
-    # Create logger
     setup_logger(log_level)
 
-    # Reading HuggingFace token
+    # Reading HuggingFace token and logging in to pull the dataset
     with open(args.hf_token, "r") as f:
         hf_token = f.read().strip()
-    # Login to Hugging Face Hub
     login(token=hf_token)
     HfFolder.save_token(hf_token)
 
@@ -58,18 +72,23 @@ def main():
         else "LIMICS/PARTAGES-Research-sourced"
     )
 
-    raw_dataset = load_dataset(
+    new_dataset = None
+
+    # Loading the sourced dataset and processing it to the deduplicated using a context manager for better resource efficiency
+    with load_dataset(
         "parquet",
         data_files="hf://datasets/LIMICS/PARTAGES-sourced/*/*.parquet"
         if args.make_commercial_version
         else "hf://datasets/LIMICS/PARTAGES-Research-sourced/*/*.parquet",
         split="train",
         download_mode="force_redownload",
-    )
-
-    logger.info(f"Shape of raw dataset: {raw_dataset.shape}")  # type: ignore
-    new_dataset = post_process(dataset=raw_dataset)
-    logger.info(f"Shape of deduplicated dataset: {new_dataset.shape}")  # type: ignore
+    ) as raw_dataset:  # type: ignore
+        logger.info(f"Shape of raw dataset: {raw_dataset.shape}")  # type: ignore
+        new_dataset = post_process(dataset=raw_dataset)
+        new_dataset = new_dataset.shuffle(seed=42)
+        new_dataset = new_dataset.flatten_indices(keep_in_memory=False)  # type: ignore
+        logger.info(f"Shape of deduplicated dataset: {new_dataset.shape}")  # type: ignore
+    gc.collect()
 
     logger.info(
         "Writing deduplicated dataset to %s" % "LIMICS/PARTAGES"
@@ -77,6 +96,7 @@ def main():
         else "LIMICS/PARTAGES-Research"
     )
 
+    # Writing the deduplicated dataset to the corresponding HuggingFace repository
     new_dataset.push_to_hub(  # type: ignore
         repo_id="LIMICS/PARTAGES"
         if args.make_commercial_version
