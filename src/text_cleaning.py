4e881cfe8704e214257dfca4ad648cbc782093db
Vivano <armandviolle@gmail.com>
2025-12-10 17:09:48 +0100

    Ajout dans le script - nettoyage supplémentaire sur le nombre minimum de mots entiers (i.e séquences de lettres non espacées) dans un example

--- src/text_cleaning.py
+++ src/text_cleaning.py
@@ -1,4 +1,6 @@
-# from : https://github.com/getalp/Flaubert/blob/master/tools/clean_text.py
+# ====================================================================================
+# Script taken from https://github.com/getalp/Flaubert/blob/master/tools/clean_text.py
+# ====================================================================================
 
 import logging
 import re
@@ -51,7 +53,6 @@ def normalize_unicode(text):
         The normalized text.
     """
     text = unicodedata.normalize("NFC", text)
-
     return text
 
 
@@ -73,7 +74,6 @@ def read_codepage(text, codepage="cp863"):
     """
     text = text.encode(codepage, "ignore").decode(codepage)
     text = text.encode("utf-8").decode("utf-8")
-
     return text
 
 
@@ -93,7 +93,6 @@ def rm_spaces(text):
     """
     pattern = re.compile(r"( ){2,}")
     text = re.sub(pattern, r" ", text)
-
     return text
 
 
@@ -113,11 +112,60 @@ def process_url_html(text):
     """
     pattern = re.compile(r"(?:www|http)\S+|<\S+|\w+\/*>")
     text = re.sub(pattern, "", text)
-
     return text
 
 
-def cleaner(text, rm_new_lines=False, do_lower=False):
+# ---------------------------------------------------------------------------
+# Additional cleaning script to avoid examples with very low semantical value
+# ---------------------------------------------------------------------------
+
+PUNCT_ASCII = r"!-/:-@[-`{-~'\."
+PUNCT_ASCII_PATTERN = re.compile(rf"[{PUNCT_ASCII}]")
+LETTERS = r"A-Za-zŒœÀ-ÂÇ-ÊÏÔà-âç-êïôû"
+CHAR_EXCLUDE_PATTERN = re.compile(rf"[^{LETTERS}0-9{PUNCT_ASCII}\s]")
+PUNCT_DEDUP_PATTERN = re.compile(rf"([{PUNCT_ASCII}\s])\1+")
+PUNCT_INSERT_PATTERN_A = re.compile(r"([:;,!\?\.])(?=\S)")
+PUNCT_INSERT_PATTERN_B = re.compile(r"(?<=\S)([:;!\?])")
+LONGWORDS_PATTERN = re.compile(r"\s?\w{35,}" + rf"[{PUNCT_ASCII}]?\s?")
+
+
+def _word_check_pattern(n: int):
+    return re.compile(r"^(?=(?:.*\b[{" + LETTERS + r"}]+\b){" + str(n) + "}).*$")
+
+
+def matches_word_check(text: str, n: int):
+    text_check = re.sub(PUNCT_ASCII_PATTERN, "", text.replace("\n", " "))
+    ptrn = _word_check_pattern(n)
+    return bool(re.match(ptrn, text_check))
+
+
+def clean_text_new(text: str, word_check_min: int):
+    text = text.replace("’", "'")
+
+    if not matches_word_check(text, word_check_min):
+        return ""
+    else:
+        text = re.sub(CHAR_EXCLUDE_PATTERN, "", text)
+        text = re.sub(PUNCT_DEDUP_PATTERN, r"\1", text)
+        text = re.sub(PUNCT_INSERT_PATTERN_A, r"\1 ", text)
+        text = re.sub(PUNCT_INSERT_PATTERN_B, r" \1", text)
+        text = re.sub(LONGWORDS_PATTERN, "", text)
+        return text
+
+
+# -------------------------------------------
+# Modified MAIN merging both cleaning scripts
+# -------------------------------------------
+
+
+def cleaner(
+    text: str,
+    rm_new_lines: bool = False,
+    do_lower: bool = False,
+    apply_codepage: bool = False,
+    codepage: str = "cp863",
+    word_check_min: int = 3,
+):
     """
     Clean up an input text
 
@@ -129,6 +177,12 @@ def cleaner(text, rm_new_lines=False, do_lower=False):
         If True, remove new line characters. Default is False.
     do_lower : bool, optional
         If True, convert text to lowercase. Default is False.
+    apply_codepage : bool
+        Keep only characters representable in a given codepage.
+    strict : bool
+        Apply script B's strict logic (min word count).
+    word_check_min : int
+        Minimum number of valid words for strict mode.
 
     Returns
     -------
@@ -142,13 +196,21 @@ def cleaner(text, rm_new_lines=False, do_lower=False):
     # Normalize whitespace characters and remove carriage return
     if rm_new_lines:
         remap = {ord("\f"): " ", ord("\r"): "", ord("\n"): "", ord("\t"): ""}
-        text = text.translate(remap)
     else:
         remap = {ord("\f"): " ", ord("\r"): ""}
-        text = text.translate(remap)
+    text = text.translate(remap)
 
     # Normalize URL links
     text = process_url_html(text)
+
+    # --- Addition of new text cleaning ---
+    if apply_codepage:
+        text = read_codepage(text, codepage=codepage)
+    text = text.replace("’", "'")
+    if not matches_word_check(text, word_check_min):
+        return ""
+    text = clean_text_new(text, word_check_min=word_check_min)
+    # -------------------------------------
 
     # remove multiple spaces in text
     text = rm_spaces(text)
